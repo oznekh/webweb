af346c2b87ae7994772330b24581185e16b773e8
Верховный <jonineh250@gmail.com>
2024-07-01 10:16:14 +0600

    first  commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
+
+function App() {
+
+
+  useEffect(() => {
+      tg.ready()
+  }, []);
+
+  const onClose = () => {
+    tg.close()
+  }
+
+  return (
+
+    <div className="App">
+    work work kwork
+      <button>закрать</button>
+    </div>
+  );
+}
+
+export default App;
